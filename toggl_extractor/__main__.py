0a2c41345c5511def02bbec2d70ca8853b35fc99
Catalin <catalin@vipra.tech>
2022-02-25 10:36:06 +0200

    starting working on the ticket

--- toggl_extractor/__main__.py
+++ toggl_extractor/__main__.py
@@ -2,9 +2,10 @@ import client
 
 
 def main():
-    # response = client.extractdata()
+    # response = client.authentification()
     # print(response, "response")
-    print(client.get_time_entry("2378846300"))
+    # print(client.get_time_entry("2378846300"))
+    print(client.get_all_time_entries("8242905")[1][1])
 
 
 if __name__ == "__main__":
--- toggl_extractor/client.py
+++ toggl_extractor/client.py
@@ -1,3 +1,4 @@
+import re
 import requests
 import json
 from requests.auth import HTTPBasicAuth
@@ -6,31 +7,26 @@ API_URL = "https://api.track.toggl.com/api/v8/"
 API_AUTH = HTTPBasicAuth("26cef151fee3519a5fef575ffa498cad", "api_token")
 
 # method for for each api request ( get_time_entries )
-def extractdata():
-    # change
-    received_data = requests.get(f"{API_URL}me?with_related_data=true", auth=API_AUTH)
+def authentification():
+    auth_state = False
+    received_data = requests.get(f"{API_URL}me", auth=API_AUTH)
     if received_data.status_code != 200:
         print(
             "API is not working, staus code: "
             + str(received_data.status_code)
             + received_data.text
         )
+        return auth_state
     else:
         print("API is working, staus code: " + str(received_data.status_code))
         datas = json.loads(received_data.text)
-        for key in datas["data"]:
-            value = datas["data"][key]
-            print(f"{key} = {value}")
-            print()
-    return datas["data"]["id"]
+        auth_state = True
+        return auth_state
 
 
 def get_time_entry(id):
     output = []
-    url = f"{API_URL}time_entries/{id}"
-    print(url)
-
-    time_entry = requests.get(url, auth=API_AUTH)
+    time_entry = requests.get(f"{API_URL}time_entries/{id}", auth=API_AUTH)
     if time_entry.status_code != 200:
         print(
             "API is not working, staus code: "
@@ -39,12 +35,32 @@ def get_time_entry(id):
         )
     else:
         print("API is working, staus code: " + str(time_entry.status_code))
+        output.insert(0, id)
         datas = json.loads(time_entry.text)
         for key in datas["data"]:
             value = datas["data"][key]
-            print(f"{key} = {value}")
+            # print(f"{key} = {value}")
             if key == "start":
-                output.insert(0, value)
-            if key == "duration":
                 output.insert(1, value)
+            if key == "duration":
+                output.insert(2, value)
         return output
+
+
+def get_all_time_entries(user_id):
+    output = []
+    received_data = requests.get(f"{API_URL}me?with_related_data=true", auth=API_AUTH)
+    if received_data.status_code != 200:
+        print(
+            "Connection failed, API code: "
+            + str(received_data.status_code)
+            + received_data.text
+        )
+    else:
+        print("Connection succeded, API code: " + str(received_data.status_code))
+        datas = json.loads(received_data.text)
+        for key in datas["data"]:
+            value = datas["data"][key]
+            if key == "time_entries":
+                output = value
+                return output
