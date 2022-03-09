af914d6ab92e43c1ff67770d548c443976051c2b
Catalin <catalin@vipra.tech>
2022-03-09 11:01:20 +0200

    feat VDU-1:implement command line interface

--- toggl_extractor/__main__.py
+++ toggl_extractor/__main__.py
@@ -1,9 +1,22 @@
 import processor
+import click
+
+help = "workdays = this command gives the toal worked hours of the employees inluding breaks smaller than 30 mins.Considering that in a 10 minutes break one is still in a work mindset and still solving problems, even if actually smoking / snacking / eating \f test = prints this is a test\f"
 
 
-def main():
-
-    print(processor.print_times(10))
+@click.command()
+@click.option("--option", help=help)
+@click.option(
+    "--range",
+    default=2,
+    prompt="no. of days",
+    help="the number of past days you want to check, starting from yesterday",
+)
+def main(option, range):
+    if option == "workdays":
+        processor.print_times(range)
+    elif option == "test":
+        print("this is a test")
 
 
 if __name__ == "__main__":
