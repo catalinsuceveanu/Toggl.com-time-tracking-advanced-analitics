1f837cd8bb1215d4b6b9476aa63a85da1cbe378d
Catalin <catalin@vipra.tech>
2022-03-28 20:19:14 +0300

    VDU-3 option(toggl_extractor): add option to print workdays to slack

--- toggl_extractor/__main__.py
+++ toggl_extractor/__main__.py
@@ -17,14 +17,24 @@ def cli():
     prompt="no. of days",
     help="the number of past days you want to check, starting from yesterday",
 )
-def workdays(range):
+@click.option("--slack")
+def workdays(range, slack):
 
     result = processor.get_workdays_for_users_per_day(range)
-    for day in result:
-        print(day + ":" + "\n")
-        for person in result[day]:
-            print(person + ": " + result[day][person])
-        print("\n\n")
+    if slack:
+        message = str()
+        for day in result:
+            message = message + str(day + ":" + "\n")
+            for person in result[day]:
+                message = message + str(person + ": " + result[day][person] + "\n")
+            message = message + str("\n\n")
+        processor.print_output_to_slack(message)
+    else:
+        for day in result:
+            print(day + ":" + "\n")
+            for person in result[day]:
+                print(person + ": " + result[day][person])
+            print("\n\n")
 
 
 cli.add_command(workdays)
