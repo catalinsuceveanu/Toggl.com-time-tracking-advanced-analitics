d4294e421bf02bf166730e4cfef20968cba0cbbb
Catalin <catalin@vipra.tech>
2022-03-29 17:33:10 +0300

    VDU-3 refactor(toggl_extractor): correct the address of the json files

--- tests/test_processor.py
+++ tests/test_processor.py
@@ -86,12 +86,12 @@ class testProcessor(unittest.TestCase):
 
     def test_convert_workdays_for_user_per_day_to_string(self):
         with open(
-            "/home/catalin/Documents/vipra-data-unity/tests/data/convert_workdays_for_user_per_day_to_string_input.json"
+            "data/convert_workdays_for_user_per_day_to_string_input.json"
         ) as input_file:
             input = eval(input_file.read())
 
         with open(
-            "/home/catalin/Documents/vipra-data-unity/tests/data/convert_workdays_for_user_per_day_to_string_output.json"
+            "data/convert_workdays_for_user_per_day_to_string_output.json"
         ) as output_file:
             output = eval(output_file.read())
 
