4bcfcff947ea0b476a5f2c40675e979b66e94b5c
Catalin <catalin@vipra.tech>
2022-04-08 14:01:05 +0300

    VDU-2 feat(toggl_extractor): add efficiency command with slack option

--- toggl_extractor/__main__.py
+++ toggl_extractor/__main__.py
@@ -15,7 +15,7 @@ def cli():
     prompt="no. of days",
     help="the number of past days you want to check, starting from yesterday",
 )
-@click.option("--slack", default=False)
+@click.option("--slack", is_flag=True)
 def workdays(range, slack):
     result = processor.get_workdays_for_users_per_day(range, slack)
     if slack:
@@ -24,7 +24,24 @@ def workdays(range, slack):
         print(result)
 
 
+@click.command()
+@click.option(
+    "--range",
+    "--r",
+    default=2,
+    help="the number of past days you want to check, starting from yesterday",
+)
+@click.option("--slack", is_flag=True)
+def efficiency(range, slack):
+    result = processor.get_efficiency_percentage_per_user_per_day(range, slack)
+    if slack:
+        print("The output for the required days was posted on slack")
+    else:
+        print(result)
+
+
 cli.add_command(workdays)
+cli.add_command(efficiency)
 
 
 if __name__ == "__main__":
